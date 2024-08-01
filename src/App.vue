6f388be4a6a487ac2e5412ec822cb4565d9cdc4a
Alex Ur <ru.booking@yandex.ru>
2024-08-01 15:16:03 +0300

    add HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+    import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
- <div> 
+    <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-xl mt-14"> 
+       <HeaderComponent/>
     </div>
 </template>
 
