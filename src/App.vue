086a33061078dc50c9477d31034b40bb70b4e0e6
Alex Ur <ru.booking@yandex.ru>
2024-08-02 13:51:32 +0300

    Add CardListComponent

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue'
-import CardComponent from './components/CardComponent.vue'
+import CardListComponent from './components/CardListComponent.vue'
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <HeaderComponent />
-
     <div class="p-10">
       <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-      <div class="grid grid-cols-4 gap-5">
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-      </div>
+      <CardListComponent />
     </div>
   </div>
 </template>
---
+++ src/components/CardListComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import CardComponent from './CardComponent.vue'
+
+const onClickAdd = () => {
+  console.log('Added')
+}
+
+const onClickFavorite = () => {
+  console.log('Add to favorites')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <CardComponent
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="1285"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite"
+    />
+  </div>
+</template>
