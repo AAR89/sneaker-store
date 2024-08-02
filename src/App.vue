2f2c93fa0982c6134898edad6017af58991c5625
Alex Ur <ru.booking@yandex.ru>
2024-08-02 12:53:59 +0300

    add CardComponent

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
-    import HeaderComponent from './components/HeaderComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue'
+import CardComponent from './components/CardComponent.vue'
 </script>
 
 <template>
-    <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-xl mt-14"> 
-       <HeaderComponent/>
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <HeaderComponent />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="grid grid-cols-4 gap-5">
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+      </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img src="/like-2.svg" alt="Like 1 logo" class="absolute top-8 left-8" />
+    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker logo" />
+    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>1285 руб.</b>
+      </div>
+      <img src="/plus.svg" alt="Plus logo" />
+    </div>
+  </div>
+</template>
