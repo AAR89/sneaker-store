2a536a73d02669d7100f0d92ca6461a5907d2f5e
Alex Ur <ru.booking@yandex.ru>
2024-08-02 18:31:27 +0300

    Add Axios for card render

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
 import HeaderComponent from './components/HeaderComponent.vue'
 import CardListComponent from './components/CardListComponent.vue'
 import DrawerComponent from './components/DrawerComponent.vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://8e61f9ea046fe2d1.mokky.dev/items')
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
 </script>
 
 <template>
@@ -10,8 +24,26 @@ import DrawerComponent from './components/DrawerComponent.vue'
     <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
       <HeaderComponent />
       <div class="p-10">
-        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-        <CardListComponent />
+        <div class="flex justify-between items-center">
+          <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+          <div class="flex gap-4">
+            <select class="py-2 px-3 border rounded-md outline-none" name="select-product" id="1">
+              <option value="По названию">По названию</option>
+              <option value="По цене (сначала дешевые)">По цене (сначала дешевле)</option>
+              <option value="По цене (сначала дороже)">По цене (сначала дороже)</option>
+            </select>
+            <div class="relative">
+              <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+              <input
+                class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400 transition"
+                type="text"
+                placeholder="Поиск..."
+              />
+            </div>
+          </div>
+        </div>
+        <CardListComponent :items="items" />
       </div>
     </div>
   </div>
--- src/components/CardListComponent.vue
+++ src/components/CardListComponent.vue
@@ -1,5 +1,8 @@
 <script setup>
 import CardComponent from './CardComponent.vue'
+defineProps({
+  items: Array
+})
 
 const onClickAdd = () => {
   console.log('Added')
@@ -11,34 +14,17 @@ const onClickFavorite = () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5">
-    <CardComponent
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="1285"
-      :onClickAdd="onClickAdd"
-      :onClickFavorite="onClickFavorite"
-    />
-    <CardComponent
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="1285"
-      :onClickAdd="onClickAdd"
-      :onClickFavorite="onClickFavorite"
-    />
-    <CardComponent
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="1285"
-      :onClickAdd="onClickAdd"
-      :onClickFavorite="onClickFavorite"
-    />
-    <CardComponent
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="1285"
-      :onClickAdd="onClickAdd"
-      :onClickFavorite="onClickFavorite"
-    />
+  <div class="mt-10">
+    <div class="grid grid-cols-4 gap-5">
+      <CardComponent
+        v-for="item in items"
+        :key="item.id"
+        :title="item.title"
+        :price="item.price"
+        :imageUrl="item.imageUrl"
+        :onClickAdd="onClickAdd"
+        :onClickFavorite="onClickFavorite"
+      />
+    </div>
   </div>
 </template>
